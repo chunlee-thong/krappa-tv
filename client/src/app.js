749532fa459e83734af14bec63905da57f0a117f
Chunlee Thong <chunlee.thong@gmail.com>
2021-11-06 05:05:21 +0700

    Update stream domain

--- client/src/app.js
+++ client/src/app.js
@@ -3,7 +3,7 @@ import { ReactFlvPlayer } from "react-flv-player";
 
 const App = () => {
   return (
-    <div style={{ textAlign: "center" }}>
+    <div style={{ textAlign: "center", padding: "1rem 3rem" }}>
       <h1>Welcome to Krappa TV</h1>
       {/* <ReactHlsPlayer
         src="http://localhost:8000/live/chunlee.m3u8"
@@ -13,10 +13,8 @@ const App = () => {
         height="auto"
       /> */}
       <ReactFlvPlayer
-        url="http://178.128.97.162:3333/live/krappa.flv"
-        heigh="800px"
-        width="800px"
-        isMuted={true}
+        url="https://krappa-tv-stream.chunleethong.com/live/krappa.flv"
+        isMuted={false}
       />
     </div>
   );
